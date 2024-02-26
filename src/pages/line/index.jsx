edbb6013b0da079b2d1f4b444dd0ccd8a74ab8d8
Preksha Gupta <prekshagupta03@gmail.com>
2024-02-26 18:15:01 +0530

    creation of barchart , piechart and linechart with nivo library

--- src/pages/line/index.jsx
+++ src/pages/line/index.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+import LineChart from "../../components/LineChart";
 
-const index = () => {
+const Line = () => {
   return (
-    <div>index</div>
-  )
-}
+    <Box m="20px">
+      <Header title="Line Chart" subtitle="Simple Line Chart" />
+      <Box height="75vh">
+        <LineChart />
+      </Box>
+    </Box>
+  );
+};
 
-export default index
+export default Line;
